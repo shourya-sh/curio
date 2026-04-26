55885213f7726dd75f3c1bac2a9cfbcea05f2d29
Akash Kothari <akash@kotharigroup.com>
2026-04-26 16:31:51 -0400

    reusable logger

---
+++ backend/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+def get_logger(name):
+    return logging.getLogger(name)
